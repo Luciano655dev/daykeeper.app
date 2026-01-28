748cb7ad895b98f7ebcf40f390b8366ed89a03b9
Luciano655dev <lucianomenezes655@gmail.com>
2026-01-28 02:41:29 -0500

    forgot that now I have to change the api url manually im tired bro I want to sleep

--- config.ts
+++ config.ts
@@ -1,2 +1,2 @@
-//export const API_URL = 'https://daykeeper-api.onrender.com'
- export const API_URL = 'http://localhost:3001'
+export const API_URL = 'https://daykeeper-api.onrender.com'
+// export const API_URL = 'http://localhost:3001'
